76237afabb2f7e880de0003bd555b61300946ac0
AdamBarsk <adambarsk@gmail.com>
2017-01-23 11:25:36 +0100

    columnlist

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import Column from './Column';
-import Card from './card';//bara för test
-=======
 import ColumnList from './ColumnList';
->>>>>>> d9c52403b58e7eea987d22f5f3b1d1446e8db2e9
+
 
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(

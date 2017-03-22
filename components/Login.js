779cfeca6b1f779697da9a4c1a7c473053d27c72
Jacob Fredriksson <jacobfredriksson@live.se>
2017-03-22 10:42:38 +0100

    login css, retrospective modal bugg fix

--- components/Login.js
+++ components/Login.js
@@ -15,12 +15,14 @@ export default class Login extends Component {
 
   render() {
     return (
-      <form onSubmit={(event) => this.handleSubmit(event)} className="login-form">
-        <input key={1} className="login-input" placeholder="Username" type="text" ref={(username) => this.username = username}/> &nbsp;
-        <input key={2} className="login-input" placeholder="Password" type="password" ref={(password) => this.password = password}/> &nbsp;
-        <Input key={3} type="submit" value="Login" className="btn btn-default login-button"/>
-        <h2 className="login-message">{this.props.loginmessage.text}</h2>
-      </form>
+      <div>
+        <form onSubmit={(event) => this.handleSubmit(event)} className="login-form">
+          <input key={1} className="login-input" placeholder="Username" type="text" ref={(username) => this.username = username}/> &nbsp;
+          <input key={2} className="login-input" placeholder="Password" type="password" ref={(password) => this.password = password}/> &nbsp;
+          <Input key={3} type="submit" value="Login" className="btn btn-default login-button"/>
+          <h2 className="login-message">{this.props.loginmessage.text}</h2>
+        </form>
+      </div>
     );
   }
 }
